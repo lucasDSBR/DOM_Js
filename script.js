6c1a300200d83e8eb891b2da89e896a0dfbdbf5a
Lucas Silva <76401911+lucasDSBR@users.noreply.github.com>
2022-10-09 15:17:40 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//Seleciona pelo ID 
+const animaisSection = document.getElementById('animais');
+const contatoSection = document.getElementById('contato');
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+const gridSectionNode = document.querySelectorAll(".grid-secton");
